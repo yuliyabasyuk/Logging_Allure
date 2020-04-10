e720cd0c9080d80866558346e4cebde236c325e0
Yuliia-Basiuk1 <yuliia.basiuk1@ibm.com>
2020-04-10 12:30:20 +0300

    Remove @Override

--- src/main/java/com/mkyong/utils/TestListener.java
+++ src/main/java/com/mkyong/utils/TestListener.java
@@ -7,43 +7,36 @@ import org.testng.ITestResult;
 
 public class TestListener implements ITestListener {
 
-    @Override
     public void onFinish(ITestContext arg0) {
         // TODO Auto-generated method stub
 
     }
 
-    @Override
     public void onStart(ITestContext arg0) {
         // TODO Auto-generated method stub
 
     }
 
-    @Override
     public void onTestFailedButWithinSuccessPercentage(ITestResult arg0) {
         // TODO Auto-generated method stub
 
     }
 
-    @Override
     public void onTestFailure(ITestResult iTestResult) {
         saveTextLog(getTestMethodName(iTestResult) + " failed and screenshot taken!");
 
     }
 
-    @Override
     public void onTestSkipped(ITestResult arg0) {
         // TODO Auto-generated method stub
 
     }
 
-    @Override
     public void onTestStart(ITestResult arg0) {
         // TODO Auto-generated method stub
 
     }
 
-    @Override
     public void onTestSuccess(ITestResult arg0) {
         // TODO Auto-generated method stub
 
